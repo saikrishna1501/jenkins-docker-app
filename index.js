eb662c88d6c7aea88083ec1febee03ebce7a214c
saikrishna1501 <saikrishna1501@gmail.com>
2022-08-14 14:51:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+express = require("express")
+app = express()
+
+port = 3000
+
+app.get('/getList',(req,res) => {
+    arr = ["phones","tvs"]  
+    res.send(arr)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
